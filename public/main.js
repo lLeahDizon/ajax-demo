a3964929c553cfa5dc88f3861e8591caa69147e0
Lemon <516330797@qq.com>
2020-09-25 11:47:05 +0800

    修改请求状态的监听

--- public/main.js
+++ public/main.js
@@ -5,8 +5,11 @@ getHTML.onclick = () => {
   request.open("GET", "/3.html");
   request.onload = () => {
     console.log(request.response);
+    // 创建div标签
     const div = document.createElement("div");
+    // 填写div内容
     div.innerHTML = request.response;
+    // 插到身体里面
     document.body.appendChild(div);
   };
   request.onerror = () => {};
@@ -35,16 +38,21 @@ getJS.onclick = () => {
 getCSS.onclick = () => {
   const request = new XMLHttpRequest();
   request.open("GET", "/style.css");
-  request.onload = () => {
-    // 创建style标签
-    const style = document.createElement("style");
-    // 填写style内容
-    style.innerHTML = request.response;
-    // 插到头里面
-    document.head.appendChild(style);
-  };
-  request.onerror = () => {
-    console.log("失败了");
+  request.onreadystatechange = () => {
+    // 下载完成，但不知道是成功 2xx 还是失败 4xx 5xx
+    if (request.readyState === 4) {
+      console.log("下载完成");
+      if (request.status >= 200 && request.status < 300) {
+        // 创建style标签
+        const style = document.createElement("style");
+        // 填写style内容
+        style.innerHTML = request.response;
+        // 插到头里面
+        document.head.appendChild(style);
+      } else {
+        alert("加载 CSS 失败");
+      }
+    }
   };
   request.send();
 };
